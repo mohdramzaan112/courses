f53a873dd838b2582d1d78dacdb4cd92507115d7
Mohammed Ramzan <35739397+mohdramzaan112@users.noreply.github.com>
2020-04-30 13:38:41 +0530

    Create form.php

---
+++ javascript/06/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>form</title>
+	</head>
+	<body>
+		<h1>Login Form</h1>
+		<form action="server.php" method="post">
+			Username:<input type="text" name="username" />
+			<br /><br />Password:<input
+				type="password"
+				name="password"
+			/><br /><br />
+			<button type="submit">Submit</button>
+		</form>
+	</body>
+</html>
